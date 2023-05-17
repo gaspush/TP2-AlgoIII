e7022bcc758d0e0b40926f741073c00dc665a709
gaspush <pizzogaspar@gmail.com>
2023-05-17 16:57:49 -0300

    jeje

--- ej1.cpp
+++ ej1.cpp
@@ -1,74 +1,61 @@
 #include <iostream>
 #include <vector>
+#include <tuple>
 using namespace std;
 
-vector<vector<int>> treeEdges;
 vector<vector<int>> aristas;
 vector<vector<int>> adyacencias;
+vector<tuple<vector<int>,bool>> treeEdges;
 int NO_LO_VI = 0, EMPECE_A_VER = 1,TERMINE_DE_VER = 2, n, m;
-vector<int> memo;
 vector<int> estado;
-
-int cubren(int v, int p = -1) {
-    if (memo[v] != -1) return memo[v];
-    int res = 0;
-    res += backConExtremoInferiorEn[v];
-    res -= backConExtremoSuperiorEn[v];
-    for (int hijo : treeEdges[v]) if (hijo != p) {
-        res += cubren(hijo, v);
-    }
-    memo[v] = res;
-    return res;
-}
-void dfs (vector<vector<int>> G, int nodo, int p ){
-    vector <int> comienzo(n,-1);
-    vector <int> fin(n,-1);
-    vector <int> padre(n,-1);
-    int i=0;
-    for (int u=0; u<G[nodo].size();u++){
-        if (padre[u]==-1){
-            treeEdges[v].push_back(u);
-            DFS-visit(G,nodo);
-        }else if (u != p){
-            backConExtremoInferiorEn[v]++;
-            backConExtremoSuperiorEn[u]++;
-        }
-    }
-}
+vector<int> padres;
 
 
-vector<vector<int>> treeEdges; //init en []
-
-
-void dfs(int v, int p){
+void DFS(int v){
     estado[v]= EMPECE_A_VER;
     for(int u: adyacencias[v]){
         if (estado[u] == NO_LO_VI){
-            treeEdges[v].push_back(u);
-            dfs(u, v);
+            padres[u] = v;
+            vector<int> tmp;
+            tmp.push_back(v);
+            tmp.push_back(u);
+            tuple<vector<int>,bool> tup = make_tuple(tmp,true);
+            treeEdges.push_back(tup);
+            DFS(u);
+        }
+        else{
+            if (u != padres[v]){    //Encontre Backedge.
+                int padre = padres[v];
+                int hijo = v;
+                while(hijo != u){
+                    for (tuple<vector<int>,bool> tup : treeEdges){
+                        if (get<0>(tup)[0] == padre && get<0>(tup)[1] == hijo){
+                            get<1>(tup) = false;
+                        }
+                    }
+                    hijo = padre;
+                    padre = padres[hijo];
+                }
+            }
+
         }
     estado[v]= TERMINE_DE_VER;
     }
-}
- 
-/*
-void DFS-visit (int u, int p){
-    i+=1;
-    comienzo[u]=k;
-    for( int v : G[u]){
-        if (padre[u]==-1){
-            padre[v]=u;
-            DFS-visit(v,V);
+    for ( tuple<vector<int>,bool> tup : treeEdges){
+        if(get<1> (tup)){
+            int tempA = BFS(get<0>(tup)[0],get<0>(tup)[1]);
+            int tempB = BFS(get<0>(tup)[1],get<0>(tup)[0]);
         }
     }
-    k+=1;
-    fin[u]=k;
-}*/
+    
+}
+
 
 void main(){
     cin >> n >> m;
 
     estado = vector<int>(n,NO_LO_VI);
+    padres = vector<int>(n,-1);
 
     for (int i = 0; i < m; i++){
         int u, v;
@@ -82,4 +69,5 @@ void main(){
         adyacencias[v].push_back(u);
     }
 
+    DFS(1);
 }
