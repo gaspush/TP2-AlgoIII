4f1711c814053d13f22f03013ae0bb1d9f20c65a
Brian D. Jaime <73399486+BrianDavJ@users.noreply.github.com>
2023-05-19 21:00:36 -0300

    Update ej1.cpp :(
    roto

--- ej1.cpp
+++ ej1.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <vector>
 #include <tuple>
+#include <iomanip>
 using namespace std;
-
+vector<int> componentes_Conexas;
 vector<vector<int>> aristas;
 vector<vector<int>> adyacencias;
 vector<tuple<vector<int>,bool>> treeEdges;
 int NO_LO_VI = 0, EMPECE_A_VER = 1,TERMINE_DE_VER = 2, n, m;
 vector<int> estado;
+vector<int> esta2;
 vector<int> padres;
 
 
@@ -24,6 +26,7 @@ void DFS(int v){
             DFS(u);
         }
         else{
+            if(estado[u]==TERMINE_DE_VER)continue;
             if (u != padres[v]){    //Encontre Backedge.
                 int padre = padres[v];
                 int hijo = v;
@@ -37,25 +40,82 @@ void DFS(int v){
                     padre = padres[hijo];
                 }
             }
-
         }
+    }
     estado[v]= TERMINE_DE_VER;
-    }
-    for ( tuple<vector<int>,bool> tup : treeEdges){
-        if(get<1> (tup)){
-            int tempA = BFS(get<0>(tup)[0],get<0>(tup)[1]);
-            int tempB = BFS(get<0>(tup)[1],get<0>(tup)[0]);
+}
+
+int DFS2(int v, int tam_cc){
+    int temp_tam=tam_cc;
+    esta2[v]= EMPECE_A_VER;
+    for(int u: adyacencias[v]){
+        if (esta2[u] == NO_LO_VI){
+            temp_tam++;
+            temp_tam=DFS2(u,temp_tam);
         }
     }
-    
+    esta2[v]= TERMINE_DE_VER;
+    return temp_tam;
+}
+
+void hacerPiquete(){ //corta los puentes como Moyano :B
+    for(tuple<vector<int>,bool> tup : treeEdges){
+        if(get<1>(tup)){
+
+            int u = get<0>(tup)[0];
+            int v = get<0>(tup)[1];
+
+            for (auto i = adyacencias[u].begin(); i != adyacencias[u].end(); i++){
+                if (*i == v){
+                    adyacencias[u].erase(i);
+                }
+            }
+            for (auto i = adyacencias[v].begin(); i != adyacencias[v].end(); i++){
+                if (*i == u){
+                    adyacencias[v].erase(i);
+                }
+            }
+        }
+    }
+}
+void armarComponentes(){
+    for(int i = 1; i <= n; i ++){
+        if (estado[i] == NO_LO_VI){
+            DFS (i);
+        }
+    }
+    hacerPiquete();
+    for(int i = 1; i <= n; i ++){
+        if(esta2[i] == NO_LO_VI){
+           int tamCC = DFS2(i,1);
+           componentes_Conexas.push_back(tamCC);
+        }
+    }
+}
+
+double fact(int i){
+    if(i == 2) return 2.0;
+    return fact(i-1)*i;
+}
+
+double comb(int i){
+    if (i == 1) return 0;
+    int factMenosDos = fact(i-2);
+    int res = (i*(i-1)*factMenosDos/(2*factMenosDos));
+    return res;
 }
 
 
-void main(){
+
+int main(){
     cin >> n >> m;
 
-    estado = vector<int>(n,NO_LO_VI);
-    padres = vector<int>(n,-1);
+    double eleccionesTotales = (n*(n-1)/2);
+
+    estado = vector<int>(n+1,NO_LO_VI);
+    esta2 = vector<int>(n+1,NO_LO_VI);
+    padres = vector<int>(n+1,-1);
+    adyacencias = vector<vector<int>>(n+1);
 
     for (int i = 0; i < m; i++){
         int u, v;
@@ -68,6 +128,13 @@ void main(){
         adyacencias[u].push_back(v);
         adyacencias[v].push_back(u);
     }
+    armarComponentes();
+    double ganar = 0;
+    for (int i = 0; i < componentes_Conexas.size(); i++){
+        ganar += comb(componentes_Conexas[i]);
+    }
 
-    DFS(1);
+    double probPerder = 1- (ganar/eleccionesTotales);
+    cout << setprecision(6)<< probPerder << endl;
+    return 0;
 }
