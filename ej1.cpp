51836b7b53b15d207da0a7342da9f620383faaae
Brian D. Jaime <73399486+BrianDavJ@users.noreply.github.com>
2023-05-22 22:17:01 -0300

    funciona, sigue sin pasar por tiempos :))))))))

--- ej1.cpp
+++ ej1.cpp
@@ -4,14 +4,15 @@
 #include <iomanip>
 #include <map>
 using namespace std;
+typedef long long ll;
 
 int NO_LO_VI = 0, EMPECE_A_VER = 1,TERMINE_DE_VER = 2, n, m;
 vector<int> componentesConexas;// Aqui alamcenaremos los tamanios de las distintas CC una vez "eliminados" los puentes
 vector<vector<int>> adyacencias;
 map<vector<int>,bool> puentes;// Mapa de aristas a bool. Si value de una arista es true, esta es puente en mi arbol DFS
 vector<int> estado;
-int tin; // ENTRO AL ÁRBOL EN QUÉ ITERACION 
-int low; // PUEDO VISITAR UN NODO ANTERIOR DEL ÁRBOL? SI EL VALOR DE CUANDO ENTRÓ N NO ES EL MISMO QUE EL VALOR QUE PUEDO VISITAR ES PORQUE VISITA UNO ANTERIOR
+vector<int> tin; // ENTRO AL ÁRBOL EN QUÉ ITERACION 
+vector<int> low; // PUEDO VISITAR UN NODO ANTERIOR DEL ÁRBOL? SI EL VALOR DE CUANDO ENTRÓ N NO ES EL MISMO QUE EL VALOR QUE PUEDO VISITAR ES PORQUE VISITA UNO ANTERIOR
 int compConexa=0;
 int it=0;
             
@@ -20,13 +21,12 @@ void marcarPuentes(int v, int p){ //re corre el grafo como dfs marcando las aris
     it++;
     tin[v] = low[v] = it;  // tin = iteración en la que entra al árbol ése nodo. Low = nodo más cercano a V que se puede alcanzar con ése nodo.
     for(int u: adyacencias[v]){  // O(m)
-        if(u == p) continue; //
+        if(u == p) continue;
         if (estado[u] == NO_LO_VI){
-            
             marcarPuentes(u, v); //sigo bajando marcando en que iteración entran por primera vez
-            low[v] = min(low[u], low[v]);// O(1)
+            low[v] = min(low[u], low[v]);//
             
-            if(low[u] > low[v]){ // El nodo hijo "u" sólo alcanza nodos posteriores, no hay backedge que cubra a V.
+            if(low[u] > low[v]){ // El nodo hijo "u" sólo alcanza nodos posteriores, no hay backedge que cubra a V entonces es puente.
                 puentes[{u, v}] = puentes[{v, u}] = true;
             }
         }
@@ -38,21 +38,21 @@ void marcarPuentes(int v, int p){ //re corre el grafo como dfs marcando las aris
 }
 int DFS2(int v, int tam_cc){
     int temp_tam=tam_cc;
-    esta2[v]= EMPECE_A_VER;
+    estado[v]= EMPECE_A_VER;
     for(int u: adyacencias[v]){
-        if(estado[v]==NO_LO_VI && !puentes[{u, v}]){//!puentes[{u,v}]){//Ignora los nodos conectados por aristas puente
+        if(estado[u]==NO_LO_VI && !puentes[{u, v}]){//!puentes[{u,v}]){//Ignora los nodos conectados por aristas puente
             temp_tam++;
             temp_tam=DFS2(u,temp_tam);
         }
     }
-    esta2[v]= TERMINE_DE_VER;
+    estado[v]= TERMINE_DE_VER;
     return temp_tam;
 }
 
 void armarComponentes(){
     for(int i = 1; i <= n; i ++){
         if(estado[i] == NO_LO_VI){
-           int tamCC = DFS(i,1);
+           int tamCC = DFS2(i,1);
            componentesConexas.push_back(tamCC);
         }
     }
@@ -64,8 +64,12 @@ int main(){
     cin >> n >> m;
     adyacencias = vector<vector<int>>(n+1);
     componentesConexas = vector<int>(n+1);
-    double eleccionesTotales = (n*(n-1)/2);
     estado = vector<int>(n+1,NO_LO_VI);
+    tin=vector<int>(n);
+    low=vector<int>(n);
+    
+    double eleccionesTotales = (n*(n-1)/2);
+    
     while (m--){
         int u, v;
         cin >> u >> v;
@@ -83,10 +87,10 @@ int main(){
 
     double ganar = 0;
     for (int i = 0; i < componentesConexas.size(); i++){
-        ganar += combinatorio(componentesConexas[i]);
+        ganar += combinatorio(componentesConexas[i])/eleccionesTotales;
     }
 
-    double probPerder = 1 - (ganar/eleccionesTotales);
+    double probPerder = 1 - (ganar);
     cout << fixed << setprecision(5) << probPerder << endl;
     return 0;
 }
