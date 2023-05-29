1b9049c372be8fcfec93acc8cc249da60cdb3e56
gaspush <pizzogaspar@gmail.com>
2023-05-29 16:11:08 -0300

    time limit exceeded

--- ej2.cpp
+++ ej2.cpp
@@ -5,13 +5,14 @@
 #include <tuple>
 #include <iomanip>
 #include <map>
+#include <algorithm>
 #include <cmath>
 #include <math.h>
 
 using namespace std;
 vector<vector<int>> adyacencias;
 vector<vector<int>> adyacenciasT;
-vector<pair<int,vector<int>>> compFC;
+vector<pair<vector<int>,vector<int>>> compFC;
 vector<bool> visitados;
 stack<int> S, ST;
 long n, m;
@@ -84,25 +85,47 @@ int main(){
                 aux.push_back(next);
             }
 
-            int menor = aux[0];
+            sort(aux.begin(),aux.end());
             for (int i = 0; i < aux.size(); i++){
                 int nodotemp = aux[i];
-                if (nodotemp < menor) menor = nodotemp;
                 for (int j = 0; j < adyacencias[nodotemp].size(); j++){                    
                     int potencial = adyacencias[nodotemp][j];
-                    if (pertence(vecinos,potencial) || pertence(aux,potencial)) break;
+                    if (pertence(vecinos,potencial) || pertence(aux,potencial)) continue;
                     vecinos.push_back(potencial);
                 }                
             }
 
-            pair<int,vector<int>> par = make_pair(menor,vecinos);
+            pair<vector<int>,vector<int>> par = make_pair(aux,vecinos);
             compFC.push_back(par);
             aux.clear();
             vecinos.clear();
         }
     }
 
+    for (int i = compFC.size()-1; i >= 0; i--){                         //Por cada componente fuertemente conexa...
+        pair<vector<int>,vector<int>> comp = compFC[i];
+        for (int v = 0; v < comp.second.size(); v++){                   //reviso todos los nodos a los que alcanzo desde alguno de sus miembros...
+            int vecino = comp.second[v];
+            for (int j = i; j < compFC.size(); j++){                    //y busco una CFC que contenga a ese nodo vecino
+                if (pertence(compFC[j].first,vecino)){                  //si encuentro una CFC que lo contenga, la elimino
+                    compFC.erase(compFC.begin()+j);                     //para quedarme solo con las CFC no alcanzables desde otras
+                }
+            }
+        }
+    }
+
+    vector<int> representantes;
+    for(int i = 0; i < compFC.size(); i++){
+        representantes.push_back(compFC[i].first[0]);
+    }
+    sort(representantes.begin(),representantes.end());
+
+    cout << representantes.size() << endl;
     
+    for (int i = 0; i < representantes.size()-1; i++){
+        cout << representantes[i] << " ";
+    }
+    cout << representantes[representantes.size()-1];
 
     return 0;
 }
